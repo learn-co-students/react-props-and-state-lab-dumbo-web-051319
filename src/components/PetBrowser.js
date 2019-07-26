0514a86a9bb7982cc514bcd45c60e4291c7549c8
DDuongNguyen <duong.nguyen4991@gmail.com>
2019-07-26 09:57:27 -0400

    Done.

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -3,8 +3,30 @@ import React from 'react'
 import Pet from './Pet'
 
 class PetBrowser extends React.Component {
+
+  // state = {
+  //   isAdopted: ''
+  // }
+  //
+  // onAdopt = () => {
+  //   this.setState({
+  //     isAdopted:
+  //   })
+  // }
+
+  loadPets= () => {
+    return this.props.data.map(pet => {
+      return (<Pet
+      key={pet.id}
+      pet={pet}
+      onAdopt={this.props.onAdopt}/>)
+    })
+  }
+
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+    return <div className="ui cards">
+    {this.loadPets()}
+    </div>
   }
 }
 
